47984fc4f93ae27f4c1ba2a16720cd5e2e8d1ec1
Bernard497-debugger <botsile55@gmail.com>
2025-09-12 16:05:50 +0200

    Delete app.py

--- app.py
+++
@@ -1,6 +0,0 @@
-DB_USERNAME=<your_postgres_user>
-DB_PASSWORD=<your_postgres_pass>
-DB_HOST=<your_postgres_host>
-DB_PORT=5432
-DB_NAME=<your_postgres_db>
-CLOUDINARY_URL=cloudinary://<your_api_key>:<your_api_secret>@dflyle0ic
